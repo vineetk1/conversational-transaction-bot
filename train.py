014178fb87c217bd9190ce375a9d61ada4d0a41b
vineetk1 <vineetchandhok@gmail.com>
2020-10-20 10:59:58 -0400

    added main() and import dictLoggingConfig

--- train.py
+++ train.py
@@ -5,12 +5,21 @@ Vineet Kumar, sioom.ai
 import pytorch_lightning as pl
 from ctbData import ctbData
 from ctbModel import ctbModel
+import logging
 import utils.logging_config
 
+logger = logging.getLogger(__name__)
 
-data = ctbData()
-data.prepare_data()
-data.setup()
-model = ctbModel()
-trainer = pl.Trainer(gpus=1)
-trainer.fit(model, datamodule=data)
+
+def main():
+    logger.debug('')
+    data = ctbData()
+    data.prepare_data()
+    data.setup()
+    model = ctbModel()
+    trainer = pl.Trainer(gpus=1)
+    trainer.fit(model, datamodule=data)
+
+
+if __name__ == '__main__':
+    main()
