863cba6afe35d9a93d60d155f68078bf00908f27
vineetk1 <vineetchandhok@gmail.com>
2020-10-23 18:56:36 -0400

    added ArgumentParser()

--- train.py
+++ train.py
@@ -7,19 +7,31 @@ from ctbData import ctbData
 from ctbModel import ctbModel
 import logging
 import utils.logging_config
+from argparse import ArgumentParser
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 logger = logging.getLogger(__name__)
 
 
-def main():
+def main(args):
     logger.debug('')
-    data = ctbData()
+    pl.seed_everything(63)
+    data = ctbData(args)
     data.prepare_data()
     data.setup()
-    model = ctbModel()
-    trainer = pl.Trainer(gpus=1)
+    model = ctbModel(args)
+    trainer = pl.Trainer.from_argparse_args(args)
     trainer.fit(model, datamodule=data)
 
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser()
+    parser.add_argument('--model', type=str, default='gpt2')
+    parser.add_argument('--tokenizer', type=str, default='gpt2')
+    parser.add_argument(
+        '--default_format_path',
+        type=str,
+        default='data/dialog-bAbI-tasks/dstc2/defaultFormat.train')
+    parser = pl.Trainer.add_argparse_args(parser)
+    args = parser.parse_args()
+    main(args)
