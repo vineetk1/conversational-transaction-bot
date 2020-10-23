7f3b7d7426b7db4aa1d6d038272d83d33e887c86
vineetk1 <vineetchandhok@gmail.com>
2020-10-23 18:50:55 -0400

    added call to utils/defaultFormat_to_gpt2Format(); removed call to dstc2_to_defaultFormat()

--- ctbData.py
+++ ctbData.py
@@ -5,24 +5,27 @@ Vineet Kumar, sioom.ai
 import torch
 from torch.utils.data import TensorDataset, RandomSampler, DataLoader, random_split
 import pytorch_lightning as pl
-from transformers.data.processors.glue import MnliProcessor
-from utils.dstc2_to_defaultFormat import dstc2_to_defaultFormat
-import os
-import urllib.request
-import zipfile
 import logging
+import sys
 
 logger = logging.getLogger(__name__)
 
 
 class ctbData(pl.LightningDataModule):
-    def __init__(self):
+    def __init__(self, args):
         logger.debug('')
         super().__init__()
+        self.args = args
 
     def prepare_data(self):
         logger.debug('')
-        dstc2_to_defaultFormat()
+        if self.args.tokenizer == "gpt2":
+            from utils.defaultFormat_to_gpt2Format import \
+                    defaultFormat_to_gpt2Format
+            defaultFormat_to_gpt2Format(self.args)
+        else:
+            logger.critical(f'unknown tokenizer: {self.args.tokenizer}')
+            sys.exit()
 
     def setup(self):
         logger.debug('')
