fbea56ce979bd4c84bf75ef6a0d776b4a44f5cf4
vineetk1 <vineetchandhok@gmail.com>
2020-10-26 13:32:01 -0400

    list of tokens that are not in tokenizer's vocabulary

---
+++ utils/NEW_TOKENS.py
@@ -0,0 +1,19 @@
+'''
+Vineet Kumar, sioom.ai
+List of new tokens that are not in tokenizer's vocabulary
+'''
+
+SPECIAL_TOKENS = {
+    'bos_token': '<BOS>',
+    'eos_token': '<EOS>',
+    'unk_token': '<UNK>',
+    'sep_token': '<SEP>',
+    'pad_token': '<PAD>',
+    'cls_token': '<CLS>',
+    'mask_token': '<MASK>'
+}
+
+TOKENS = [
+    '<SILENCE>', 'api_call', 'R_post_code', 'R_cuisine', 'R_location',
+    'R_phone', 'R_address', 'R_price', 'R_rating'
+]
