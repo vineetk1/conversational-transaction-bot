c26280fe925a8df09816242c2e2c25c5dabbf41d
vineetk1 <vineetchandhok@gmail.com>
2020-11-13 14:31:23 -0500

    init works, and empty methods are called at the right time

--- ctbModel.py
+++ ctbModel.py
@@ -8,19 +8,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 import logging
 import sys
+from typing import Dict
 
 logger = logging.getLogger(__name__)
 
 
 class ctbModel(pl.LightningModule):
-    def __init__(self, args, data_specific):
+    def __init__(self, args, len_tokenizer: int):
         super().__init__()
         logger.debug('')
         self.args = args
         if args.model == "gpt2":
             from transformers import GPT2LMHeadModel
-            self.model = GPT2LMHeadModel.from_pretrained('gpt2')
-            self.model.resize_token_embeddings(data_specific['len_tokenizer'])
+            self.model = GPT2LMHeadModel.from_pretrained('distilgpt2')
+            self.model.resize_token_embeddings(len_tokenizer)
         else:
             logger.critical(f'unknown model: {self.args.model}')
             sys.exit()
@@ -28,31 +29,31 @@ class ctbModel(pl.LightningModule):
     def forward(self):
         logger.debug('')
 
-    def training_step(self):
+    def training_step(self, batch, batch_idx):
         logger.debug('')
 
-    def training_step_end(self):
+    def training_step_end(self, batch_parts):
         logger.debug('')
 
-    def training_epoch_end(self):
+    def training_epoch_end(self, training_step_outputs):
         logger.debug('')
 
-    def validation_step(self):
+    def validation_step(self, batch, batch_idx):
         logger.debug('')
 
-    def validation_step_end(self):
+    def validation_step_end(self, batch_parts):
         logger.debug('')
 
-    def validation_epoch_end(self):
+    def validation_epoch_end(self, validation_step_outputs):
         logger.debug('')
 
-    def test_step(self):
+    def test_step(self, batch, batch_idx):
         logger.debug('')
 
-    def test_step_end(self):
+    def test_step_end(self, batch_parts):
         logger.debug('')
 
-    def test_epoch_end(self):
+    def test_epoch_end(self, test_step_outputs):
         logger.debug('')
 
     def configure_optimizers(self):
