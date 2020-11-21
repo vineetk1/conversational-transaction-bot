6f76a2f4449d9138f2042b084b97fce286c753fb
vineetk1 <vineetchandhok@gmail.com>
2020-11-21 18:26:55 -0500

    primarily changed logger to logg

--- ctbModel.py
+++ ctbModel.py
@@ -2,70 +2,57 @@
 Vineet Kumar, sioom.ai
 '''
 
-import pytorch_lightning as pl
+from pytorch_lightning import LightningModule
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import logging
 import sys
 from typing import Dict
 
-logger = logging.getLogger(__name__)
+logg = logging.getLogger(__name__)
 
 
-class ctbModel(pl.LightningModule):
+class ctbModel(LightningModule):
     def __init__(self, args, len_tokenizer: int):
         super().__init__()
-        logger.debug('')
+        logg.debug('')
         self.args = args
         if args.model == "gpt2":
             from transformers import GPT2LMHeadModel
             self.model = GPT2LMHeadModel.from_pretrained('distilgpt2')
             self.model.resize_token_embeddings(len_tokenizer)
         else:
-            logger.critical(f'unknown model: {self.args.model}')
+            logg.critical(f'unknown model: {self.args.model}')
             sys.exit()
 
     def forward(self):
-        logger.debug('')
+        logg.debug('')
 
-    def training_step(self, batch: Dict, batch_idx):
-        logger.debug('')
+    def training_step(self, batch: Dict[str, torch.Tensor], batch_idx: int):
+        logg.debug('')
         outputs = self.model(**batch, labels=batch["input_ids"])
-        loss = outputs.loss
+        loss = outputs[0]
         self.log('train_loss', loss, on_step=True, on_epoch=True,
                  prog_bar=True, logger=True)
         return loss
 
-    def training_step_end(self, batch_parts):
-        logger.debug('')
-
-    def training_epoch_end(self, training_step_outputs):
-        logger.debug('')
-
     def validation_step(self, batch, batch_idx):
-        logger.debug('')
+        logg.debug('')
         outputs = self.model(**batch, labels=batch["input_ids"])
-        loss = outputs.loss
-        self.log('val_loss', loss)
-
-    def validation_step_end(self, batch_parts):
-        logger.debug('')
-
-    def validation_epoch_end(self, validation_step_outputs):
-        logger.debug('')
+        loss = outputs[0]
+        self.log('val_loss', loss, on_step=True, on_epoch=True,
+                 prog_bar=True, logger=True)
 
     def test_step(self, batch, batch_idx):
-        logger.debug('')
+        logg.debug('')
 
     def test_step_end(self, batch_parts):
-        logger.debug('')
+        logg.debug('')
 
     def test_epoch_end(self, test_step_outputs):
-        logger.debug('')
+        logg.debug('')
 
     def configure_optimizers(self):
-        logger.debug('')
+        logg.debug('')
         return torch.optim.Adam(
             [p for p in self.parameters() if p.requires_grad],
             lr=2e-05,
