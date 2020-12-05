de5248917790c60e99b31c71420755ca0d1ffa35
vineetk1 <vineetchandhok@gmail.com>
2020-12-05 18:18:28 -0500

    added checkpoint loading and testing

--- ctbMain.py
+++ ctbMain.py
@@ -27,27 +27,43 @@ def main():
             dictionary for line in paramF if line[0] == '{'
             and isinstance(dictionary := literal_eval(line), dict)
         ]
-    seed_everything(63)
-    model = ctbModel(param_dicts[1], utils.NEW_TOKENS.SPECIAL_TOKENS,
-                     utils.NEW_TOKENS.DSTC2_TOKENS)
-    data = ctbData(param_dicts[2])
-    data.prepare_data(tokenizer=model.get_tokenizer(),
-                      model_id=model.get_model_id())
-    data.setup()
     tb_logger = TensorBoardLogger('ctb_lightning_logs',
                                   name=Path(params_file_path).name)
-    checkpoint_callback = ModelCheckpoint(
-        monitor='val_loss',
-        mode='min',
-        save_top_k=2,
-        period=1,
-        filename='{epoch:02d}-{val_loss:.4f}')
-    trainer = Trainer(logger=tb_logger,
-                      num_sanity_val_steps=0,
-                      callbacks=[checkpoint_callback],
-                      **param_dicts[3])
-    trainer.fit(model, datamodule=data)
-    trainer.test()  # auto loads checkpoint file with lowest val loss
+    if 'chkpt' in param_dicts[0] and param_dicts[0]['chkpt'] is not None:
+        model = ctbModel.load_from_checkpoint(
+            checkpoint_path=param_dicts[0]['chkpt'])
+        strng = (f'Checkpointed file has model_type='
+                 f'{model.get_model_id()["model_type"]} and tokenizer_type='
+                 f'{model.get_model_id()["tokenizer_type"]}')
+        logg.info(strng)
+        trainer = Trainer(logger=tb_logger,
+                          checkpoint_callback=False,
+                          **param_dicts[3])
+    else:
+        seed_everything(63)
+        model = ctbModel(param_dicts[1], utils.NEW_TOKENS.SPECIAL_TOKENS,
+                         utils.NEW_TOKENS.DSTC2_TOKENS)
+        checkpoint_callback = ModelCheckpoint(
+            monitor='val_loss',
+            mode='min',
+            save_top_k=2,
+            period=1,
+            filename='{epoch:02d}-{val_loss:.4f}')
+        trainer = Trainer(logger=tb_logger,
+                          num_sanity_val_steps=0,
+                          callbacks=[checkpoint_callback],
+                          **param_dicts[3])
+
+    data = ctbData(param_dicts[2])
+    data.prepare_data(tokenizer=model.get_tokenizer(),
+                      tokenizer_type=model.get_model_id()['tokenizer_type'])
+    data.setup()
+
+    if 'chkpt' in param_dicts[0] and param_dicts[0]['chkpt'] is not None:
+        trainer.test(model, test_dataloaders=data.test_dataloader())
+    else:
+        trainer.fit(model, datamodule=data)
+        trainer.test()  # auto loads checkpoint file with lowest val loss
 
 
 if __name__ == '__main__':
