56531d65bd4e249e10ce1824e60c60c9436bc9be
vineetk1 <vineetchandhok@gmail.com>
2020-12-15 21:51:35 -0500

    UNK token is not needed

--- utils/NEW_TOKENS.py
+++ utils/NEW_TOKENS.py
@@ -6,14 +6,14 @@ List of new tokens that are not in tokenizer's vocabulary
 SPECIAL_TOKENS = {
     'bos_token': '<BOS>',
     'eos_token': '<EOS>',
-    'unk_token': '<UNK>',
+    # 'unk_token': '<UNK>',
     'sep_token': '<SEP>',
     'pad_token': '<PAD>',
     # 'cls_token': '<CLS>',
     'mask_token': '<MASK>'
 }
 
-# ADDITIONAL_SPECIAL_TOKENS = ['<USER>', '<BOT>']
+# ADDITIONAL_SPECIAL_TOKENS = ['<GENERATE>']
 
 DSTC2_TOKENS = [
     '<SILENCE>', 'api_call', 'R_post_code', 'R_cuisine', 'R_location',
