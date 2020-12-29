7fc6c81cdd3f602ed20428ba543e2f2a6fd4dbbf
vineetk1 <vineetchandhok@gmail.com>
2020-12-29 16:31:40 -0500

    removed code that separated label from input

--- utils/defaultFormat_to_gpt2Format.py
+++ utils/defaultFormat_to_gpt2Format.py
@@ -95,18 +95,12 @@ def default_to_gpt2_format(tokenizer, fromFile: pathlib.PosixPath,
                 feature_ids_trunc = feature_ids['input_ids'][-(
                     tokenizer.max_model_input_sizes['distilgpt2'] -
                     labels_max_len - 3):]
-                if fromFile.suffix == '.test':
-                    # NOTE "copy.deepcopy" is not needed below
-                    lst_input_ids.append(
-                        ([tokenizer.bos_token_id] + feature_ids_trunc +
-                         [tokenizer.sep_token_id], label_ids['input_ids']))
-                else:
-                    # NOTE "copy.deepcopy" is not needed below
-                    lst_input_ids.append([tokenizer.bos_token_id] +
-                                         feature_ids_trunc +
-                                         [tokenizer.sep_token_id] +
-                                         label_ids['input_ids'] +
-                                         [tokenizer.eos_token_id])
+                # NOTE "copy.deepcopy" is not needed below
+                lst_input_ids.append([tokenizer.bos_token_id] +
+                                     feature_ids_trunc +
+                                     [tokenizer.sep_token_id] +
+                                     label_ids['input_ids'] +
+                                     [tokenizer.eos_token_id])
                 try:
                     idx = dlg['bot_idx'].index(i)
                     history = " ".join([
