9c97c1958d06a4932244133d23646add9b490bd0
vineetk1 <vineetchandhok@gmail.com>
2021-02-15 11:53:03 -0500

    adding code to get statistics on dialogs

--- ctbModel.py
+++ ctbModel.py
@@ -8,7 +8,9 @@ from logging import getLogger
 from sys import exit
 from typing import Dict, List
 from importlib import import_module
+from pathlib import Path
 import copy
+import pickle
 
 logg = getLogger(__name__)
 
@@ -24,7 +26,7 @@ class ctbModel(LightningModule):
         self.d_params = copy.deepcopy(d_params)
         # Trainer('auto_lr_find': True,...) requires self.lr
         self.lr = self.d_params['optz_params'].pop(
-                       'lr', None) if 'optz_params' in self.d_params else None
+            'lr', None) if 'optz_params' in self.d_params else None
         self.model_type = self.d_params.pop('model_type', None)
         self.tokenizer_type = self.d_params.pop('tokenizer_type', None)
 
@@ -58,7 +60,7 @@ class ctbModel(LightningModule):
     def change_hperparams(self, d_params: dict):
         self.d_params = d_params
         self.lr = self.d_params['optz_params'].pop(
-                      'lr', None) if 'optz_params' in self.d_params else None
+            'lr', None) if 'optz_params' in self.d_params else None
 
     def forward(self):
         logg.debug('')
@@ -195,20 +197,64 @@ class ctbModel(LightningModule):
 
     def set_pass_fail_stat(self):
         self.pass_fail_stat = True
+        stat_dir = Path.cwd().joinpath('statistics')
+        stat_dir.mkdir(exist_ok=True)
+        self.temp_file = stat_dir.joinpath('temp.txt')
+        self.temp_file.touch()
+        #self.temp_file.write_text('')   # empty the file
+        self.dlgs_pass_noTrunc_file = stat_dir.joinpath('dlgs_pass_noTrunc.txt')
+        self.dlgs_pass_noTrunc_file.touch()
+        self.dlgs_pass_noTrunc_file.write_text('')   # empty the file
+        self.dlgs_fail_noTrunc_file = stat_dir.joinpath('dlgs_fail_noTrunc.txt')
+        self.dlgs_fail_noTrunc_file.touch()
+        self.dlgs_fail_noTrunc_file.write_text('')   # empty the file
+        self.dlgs_pass_trunc_file = stat_dir.joinpath('dlgs_pass_trunc.txt')
+        self.dlgs_pass_trunc_file.touch()
+        self.dlgs_pass_trunc_file.write_text('')   # empty the file
+        self.dlgs_fail_trunc_file = stat_dir.joinpath('dlgs_fail_trunc.txt')
+        self.dlgs_fail_trunc_file.touch()
+        self.dlgs_fail_trunc_file.write_text('')   # empty the file
+        self.dlgs_stat_file = stat_dir.joinpath('dlgs_stat.txt')
+        self.dlgs_stat_file.touch()
+        self.dlgs_stat_file.write_text('')   # empty the file
+        self.dlgs_idxs_file = stat_dir.joinpath('dlgs_idxs.test')
+        if not self.dlgs_idxs_file.exists():
+            logg.critical(f'Following file does not exist: {self.dlgs_idxs_file}')
+            exit()
 
     def pass_fail_stat_step(self, batch: Dict[str, torch.Tensor]):
-        logg.debug('')
+        self.pass_fail_stat_end()
         return
-        model_kwargs = {
-            'attention_mask': batch['attention_mask'],
-            'token_type_ids': batch['token_type_ids']
-        }
+        # batch['model']['input_ids'] = batch of
+        #           (<BOS> + sequence + <SEP> + labels + <EOS> + <PAD>..<PAD>)
+        # batch['model']['token_type_ids'] =
+        #          batch of (ones at (<SEP> + labels + <EOS> + <PAD>..<PAD>))
+        # batch['model']['attention_mask'] =
+        #      batch of (ones at (<BOS> + sequence + <SEP> + labels + <EOS>))
+        #  batch['idxs'] = batch of (indexes of dialog-turns)
 
-        # outputs = batch['input_ids'] + (batch of generated outputs)
-        # batch['input_ids']=batch of (<BOS> + sequence + <SEP> + <PAD>..<PAD>)
+        sep_idxs = torch.count_nonzero(
+            batch['model']['token_type_ids'],
+            dim=1).sub(batch['model']['input_ids'].shape[1]).mul(-1).tolist()
+        eos_idxs = (torch.count_nonzero(batch['model']['attention_mask'],
+                                        dim=1)).sub(1).tolist()
+        sep_idx_max = max(sep_idxs)
+
+        for i, sep_idx in enumerate(sep_idxs):
+            batch['model']['attention_mask'][i, :sep_idx + 1] = 1
+            batch['model']['attention_mask'][i, sep_idx + 1:] = 0
+
+        model_kwargs = {
+            'attention_mask':
+            batch['model']['attention_mask'][:, :sep_idx_max + 1],
+            'token_type_ids':
+            batch['model']['token_type_ids'][:, :sep_idx_max + 1]
+        }
+        # outputs = batch of (['model']['input_ids'][:, :sep_idx_max+1] +
+        # predicted labels)
         outputs = self.model.generate(
             # parameter = None => replace with self.config.parameter
-            input_ids=batch['input_ids'],
+            input_ids=batch['model']['input_ids'][:, :sep_idx_max + 1],
             max_length=self.tokenizer.max_model_input_sizes['distilgpt2'],
             min_length=1,
             do_sample=False,
@@ -232,16 +278,31 @@ class ctbModel(LightningModule):
             prefix_allowed_tokens_fn=None,
             **model_kwargs)
 
-        print(
-            f"input_ids={self.tokenizer.batch_decode(batch['input_ids'], skip_special_tokens=True)}"
-        )
-        print(
-            f"labels_ids={self.tokenizer.batch_decode(batch['label_ids'], skip_special_tokens=True)}"
-        )
-        print(
-            f"outputs={self.tokenizer.batch_decode(outputs[:, batch['input_ids'].shape[1]:], skip_special_tokens=True)}"
-        )
-        print('end')
+        eos_idxs_in_predicted_labels = (outputs[:, sep_idx_max + 1:].eq(
+            self.tokenizer.eos_token_id)).nonzero(
+                as_tuple=False)[:, -1].add(sep_idx_max + 1).tolist()
+        with self.temp_file.open('a') as file:
+            for i, (sep_idx, eos_idx, eos_idx_in_predicted_label) in enumerate(
+                    zip(sep_idxs, eos_idxs, eos_idxs_in_predicted_labels)):
+                idx_dlg_turn = batch['idxs'][i]
+                exact_match = batch['model']['input_ids'][
+                    i, sep_idx + 1:eos_idx].equal(
+                        outputs[i, sep_idx_max + 1:eos_idx_in_predicted_label])
+                input_text = self.tokenizer.decode(outputs[i, 1:sep_idx])
+                actual_label = self.tokenizer.decode(
+                    batch['model']['input_ids'][i, sep_idx + 1:eos_idx])
+                predicted_label = self.tokenizer.decode(
+                    outputs[i, sep_idx_max + 1:eos_idx_in_predicted_label])
+                # line written to disk: index of dialog turn /t exact_match /t
+                # input text /t actual label text /t predicted label text
+                strng = (f'{idx_dlg_turn}\t{exact_match}\t{input_text}\t'
+                         f'{actual_label}\t{predicted_label}\n')
+                file.write(strng)
 
     def pass_fail_stat_end(self):
-        logg.debug('')
+        with self.dlgs_idxs_file.open('rb') as bfile:
+            dlgs_idxs = pickle.load(bfile)
+        #dlgs_turns = self.temp_file.read_text()
+        with self.temp_file.open('r') as dlgs_turns:
+            for dlg_turn in dlgs_turns:
+                pass
