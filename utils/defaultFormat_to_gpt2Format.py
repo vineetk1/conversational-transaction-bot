b7a4b017ef897e154e6811d6f17b747e9f1ded20
vineetk1 <vineetchandhok@gmail.com>
2021-02-25 16:05:29 -0500

    recording more meta data for dlgs and turns

--- utils/defaultFormat_to_gpt2Format.py
+++ utils/defaultFormat_to_gpt2Format.py
@@ -47,8 +47,10 @@ def defaultFormat_to_gpt2Format(tokenizer, tokenizer_type,
 
     statistics_dir = pathlib.Path.cwd().joinpath('statistics')
     statistics_dir.mkdir(exist_ok=True)
-    dlgs_idxs_file = statistics_dir.joinpath('dlgs_idxs.test')
-    dlgs_idxs_file.touch()
+    dlgs_info_test_file = statistics_dir.joinpath('dlgs_info.test')
+    dlgs_info_test_file.touch()
+    turns_info_test_file = statistics_dir.joinpath('turns_info.test')
+    turns_info_test_file.touch()
 
     # find max length of labels in train/val/test files
     labels_max_len = 0
@@ -65,7 +67,7 @@ def defaultFormat_to_gpt2Format(tokenizer, tokenizer_type,
 
     for fromFile, toFile in zip(fromFiles, toFiles):
         default_to_gpt2_format(tokenizer, fromFile, toFile, labels_max_len,
-                               dlgs_idxs_file)
+                               turns_info_test_file, dlgs_info_test_file)
     return {
         "f_paths": {
             "train": toTrainF,
@@ -77,16 +79,24 @@ def defaultFormat_to_gpt2Format(tokenizer, tokenizer_type,
 
 def default_to_gpt2_format(tokenizer, fromFile: pathlib.PosixPath,
                            toFile: pathlib.PosixPath, labels_max_len: int,
-                           dlgs_idxs_file: pathlib.PosixPath) -> None:
+                           turns_info_test_file: pathlib.PosixPath,
+                           dlgs_info_test_file: pathlib.PosixPath) -> None:
     lst_input_ids = []
-    lst_dlg_idxs = []
+    dlgs_info_test = []
+    turns_info_test = []
     test_file = fromFile.suffix == '.test'
     with fromFile.open('rb') as fromF:
         lst_dlgs = pickle.load(fromF)
         for dlg in lst_dlgs:
             assert len(dlg['user']) == len(dlg['bot'])
             if test_file:
-                lst_dlg_idxs.append(len(lst_input_ids))
+                # record: (1) line # from the original dataset file where
+                # this dlg started, (2) the index in lst_input_ids where this
+                # dlg started
+                dlgs_info_test.append(
+                    copy.deepcopy(
+                        [dlg['dlg_start_lineno'],
+                         len(lst_input_ids)]))
             # persona is not used, so it is ignored
             history = ''
             for i, (u_str, b_str) in enumerate(zip(dlg['user'], dlg['bot'])):
@@ -94,21 +104,38 @@ def default_to_gpt2_format(tokenizer, fromFile: pathlib.PosixPath,
                                         return_length=False,
                                         return_token_type_ids=False,
                                         return_attention_mask=False)
+
+                if test_file:
+                    # record whether user-str and history were untruncated
+                    # (True) or truncated (False) in this turn
+                    user_ids = tokenizer(u_str,
+                                         return_length=False,
+                                         return_token_type_ids=False,
+                                         return_attention_mask=False)
+                    turns_info_test.append([
+                        (len(user_ids['input_ids']) <=
+                         (tokenizer.max_model_input_sizes['distilgpt2'] -
+                          labels_max_len - 3)),
+                        (len(feature_ids['input_ids']) <=
+                         (tokenizer.max_model_input_sizes['distilgpt2'] -
+                          labels_max_len - 3))
+                    ])
+
                 label_ids = tokenizer(b_str,
                                       return_length=False,
                                       return_token_type_ids=False,
                                       return_attention_mask=False)
                 # Policy: If feature_ids is larger than max allowed by GPT2,
-                # then truncate by picking tokens from the end (i.e. drop the
-                # tokens in the beginning)
+                # then truncate by droping the tokens in the beginning
                 feature_ids_trunc = feature_ids['input_ids'][-(
                     tokenizer.max_model_input_sizes['distilgpt2'] -
                     labels_max_len - 3):]
-                lst_input_ids.append(copy.deepcopy([tokenizer.bos_token_id] +
-                                     feature_ids_trunc +
-                                     [tokenizer.sep_token_id] +
-                                     label_ids['input_ids'] +
-                                     [tokenizer.eos_token_id]))
+                lst_input_ids.append(
+                    copy.deepcopy([tokenizer.bos_token_id] +
+                                  feature_ids_trunc +
+                                  [tokenizer.sep_token_id] +
+                                  label_ids['input_ids'] +
+                                  [tokenizer.eos_token_id]))
                 try:
                     idx = dlg['bot_idx'].index(i)
                     history = " ".join([
@@ -117,12 +144,19 @@ def default_to_gpt2_format(tokenizer, fromFile: pathlib.PosixPath,
                     ])
                 except ValueError:
                     history = " ".join([history, u_str, b_str])
-        lst_dlg_idxs.append(len(lst_input_ids))
+        # last entry is not a new dlg but the previous dlg with the same
+        # line # but index of last turn
+        dlgs_info_test.append(
+            copy.deepcopy([dlg['dlg_start_lineno'],
+                           len(lst_input_ids)]))
 
     with toFile.open('wb') as toF:
         pickle.dump(lst_input_ids, toF, protocol=pickle.HIGHEST_PROTOCOL)
         logg.info(f'Done writing to file {toFile}')
     if test_file:
-        with dlgs_idxs_file.open('wb') as idxF:
-            pickle.dump(lst_dlg_idxs, idxF, protocol=pickle.HIGHEST_PROTOCOL)
-            logg.info(f'Done writing to file {dlgs_idxs_file}')
+        with dlgs_info_test_file.open('wb') as dF, turns_info_test_file.open(
+                'wb') as tF:
+            pickle.dump(dlgs_info_test, dF, protocol=pickle.HIGHEST_PROTOCOL)
+            logg.info(f'Done writing to file {dlgs_info_test_file}')
+            pickle.dump(turns_info_test, tF, protocol=pickle.HIGHEST_PROTOCOL)
+            logg.info(f'Done writing to file {turns_info_test_file}')
